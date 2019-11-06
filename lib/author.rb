f48d18b4b85d66918c5f23e4e9d6d3bd3f048003
Vesna Vucinic <vesna.vucinic68@gmail.com>
2019-11-06 19:04:14 +0000

    Automatically backed up by Learn

--- lib/author.rb
+++ lib/author.rb
@@ -11,4 +11,7 @@ class Author
  # Author #add_post takes in an argument of a post and associates that post with the author by telling the post that it belongs to that author
   def add_post(post)
     post.author = self
+  end
+  
+  
   
