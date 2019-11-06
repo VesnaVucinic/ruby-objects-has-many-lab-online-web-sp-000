a6db651abfdb3bf1471359087918739a7f19fda4
Vesna Vucinic <vesna.vucinic68@gmail.com>
2019-11-06 19:16:16 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,6 +1,6 @@
 class Post 
   attr_accessor :title 
-#  
+# Post @@all is a class variable set to an array 
   @@all = []
   
 #Post #new pushes new instances into a class variable called @@all upon initialization
